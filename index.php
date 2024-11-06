b7cafca2434db0ee8ab54c984de9076137158d81
tst4d <michaelworkmkt@gmail.com>
2024-11-06 17:21:51 +0700

    tambah li

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsam ad cupiditate maxime laboriosam? Ea eaque quidem ipsum consequatur consequuntur quam quia veniam, voluptates sunt velit est maiores laboriosam quae.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
